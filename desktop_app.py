95a1cd6858ef040db60df97e8710c4f1c8942310
unknown <coolcoderai123@gmail.com>
2026-03-30 14:37:14 -0700

    got it working

--- desktop_app.py
+++ desktop_app.py
@@ -1,4 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Plane App")
+        button = QPushButton("Press Me!")
+
+        self.setCentralWidget(button)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
